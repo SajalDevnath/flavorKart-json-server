e62e14a70a5b5ab2bba3c77edbe5938e4f20f9e9
SajalDevnath <sajaldevnath96@gmail.com>
2023-06-16 13:39:28 +0530

    change on server file

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-// JSON Server module 
+// JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
-// Make sure to use the default middleware 
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
-// Add this before 
-server.use(router)
+// Add this before server.use(router)
 server.use(
-    jsonServer.rewriter({
-        "/api/*": "/$1",
-    }));
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
 server.use(router);
+// Listen to port
 server.listen(3000, () => {
-    console.log("JSON Server is running");
+ console.log("JSON Server is running");
 });
+
+// Export the Server API
+module.exports = server;
